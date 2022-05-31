59118d665285ddd8cfc6238ab2c3506bbca38bf8
RiveraDenisse <denisse_alejandrar@hotmail.com>
2022-05-30 19:43:09 -0500

    create route for recipes

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express();
 app.listen(3001, () => {
   console.log("API server now on port 3001");
 });
+
+//route to request data from data.json
+const { recipes } = require("./data/data");
+
+//create route for recipes
+app.get("/recipes", (req, res) => {
+  res.json(recipes);
+});
